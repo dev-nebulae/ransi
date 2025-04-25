cac415bd3b729bdbf3a05db87cc0ec21cbb9dc2d
nebulae <mar1lusk1@proton.me>
2025-04-25 16:16:34 +0200

    Add italics

--- src/layout.rs
+++ src/layout.rs
@@ -2,6 +2,13 @@
 ///
 /// This function takes a string slice as an input and returns a new string that,
 /// when printed, will appear in bold.
+///
+/// # Examples
+///
+/// ```
+/// let important_text: &str = "This is really important!";
+/// println!("{}", bold(important_text));
+/// ```
 pub fn bold(text: &str) -> String {
     format!("\033[22m")
 }
@@ -14,7 +21,6 @@ pub fn bold(text: &str) -> String {
 /// # Examples
 ///
 /// ```
-/// // Cool Hello World
 /// use ransi::layout::blink;
 /// let text = "This text will blink";
 ///
@@ -25,4 +31,26 @@ pub fn bold(text: &str) -> String {
 /// ```
 pub fn blink(text: &str) -> String {
     format!("\033[5m{}\033[25m", text) // NOTE: use 25m to reset the blink
+}
+
+/// italic(text: &str) -> String - Makes the text italic
+///
+/// This function takes a string slice as an input and returns a new string that,
+/// when printed, is italic.
+///
+/// # Examples
+///
+/// ```
+/// use ransi::layout::italic;
+///
+/// let text1 = "Violets are blue, Roses are red, This is not italic text";
+/// let text2 = "Violets are blue, Roses are red, This is italic text";
+/// let italic_text = italic(text1);
+///
+/// println!("{}", text);
+/// println!("And now italic...");
+/// println!("{}", text2);
+/// ```
+fn italic(text: &str) -> String {
+    format!("\033[3m{}\033[23m", text)
 }
